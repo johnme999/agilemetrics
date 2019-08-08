87091602c7ba169f29c47b37396d87b9c57f00a5
Sanoop Krishnan <sanoop.krishnan@ust-global.com>
2019-08-08 22:58:16 +0530

    Initial commit

---
+++ src/main/java/com/awsomeday/agilemetricsjs3/AgilemetricsJs3Application.java
@@ -0,0 +1,13 @@
+package com.awsomeday.agilemetricsjs3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgilemetricsJs3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgilemetricsJs3Application.class, args);
+	}
+
+}
